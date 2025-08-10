05f3e0f0e021832d8fe9bea8a95d59746698aac9
Uttam Bhagwat <rohit.a.bhagwat@accenture.com>
2025-08-10 21:48:06 +0530

    Corrected code, added missing css imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./styles/LandingPage.css"; // LandingPage styles
-import "./styles/LoadingScreen.css"; // LoadingScreen styles
-import "./styles/OccasionSelect.css"; // OccasionSelect styles
+import "./styles/CreateCard.css";
 import "./styles/GeneratedCard.css"; // GeneratedCard styles
+import "./styles/GiftSuggestions.css"; // GiftSuggestions styles
+import "./styles/Home.css"; // Home styles
+import "./styles/LandingPage.css"; 
+import "./styles/LoadingScreen.css"; 
+import "./styles/OccasionSelect.css"; // OccasionSelect styles
+import "./styles/PersonalizationForm.css"; // PersonalizationForm styles
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
